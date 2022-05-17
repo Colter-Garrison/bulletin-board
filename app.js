011588d46a0218bddc295df3c4eb11fd037c4888
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-17 14:14:29 -0700

    taking a break

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import { fetchPosts } from './fetch-utils.js';
+import { renderPostEl } from './render.js';
 
 const loginButton = document.getElementById('login-button');
+const createList = document.getElementById('create-list');
+
+async function loadData() {
+    const postEl = await fetchPosts();
+
+    for (let post of postEl) {
+        const postDiv = renderPostEl(post);
+        createList.append(postDiv);
+    }
+}
+
+loadData();
 
 loginButton.addEventListener('click', () => {
-    location.replace('');
+    location.replace('./auth-page');
 });
-
-console.log(fetchPosts());
